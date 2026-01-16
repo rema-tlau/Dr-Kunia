93e10d9f7c23d9101d603351159c353d665c081c
Rema <remruatatlau9@gmail.com>
2026-01-16 23:55:43 +0530

    bind flask to 0.0.0.0 for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from healthcare_chatbot import chatbot_response
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
